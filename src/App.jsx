8c0a5dfc62472f344fc240e18d31b8aebd805281
dhivyakarthickthiru <dhivyathirumugai95@gmail.com>
2026-03-31 00:15:20 +0530

    toastify successfully done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter,RouterProvider } from "react-router"
-
+import { ToastContainer } from "react-toastify";
 
 const router=createBrowserRouter([
   {
@@ -9,7 +9,22 @@ const router=createBrowserRouter([
 ]);
 
 const App = () => {
-  return  <RouterProvider router={router} />;
-  
+  return ( 
+    <>
+  <RouterProvider router={router} />;
+    <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  )  
 }
 export default App;
